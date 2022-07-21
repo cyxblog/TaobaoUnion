657abaf5dfd6d28c92fd72146e46766b07124b74
cyx <389856005@qq.com>
2022-07-21 10:28:05 +0800

    实现搜索界面逻辑层代码

---
+++ app/src/main/java/com/example/taobaounion/presenter/SearchPresenterImpl.java
@@ -0,0 +1,45 @@
+package com.example.taobaounion.presenter;
+
+import com.example.taobaounion.view.ISearchCallback;
+
+public class SearchPresenter implements ISearchPresenter{
+    @Override
+    public void getHistories() {
+
+    }
+
+    @Override
+    public void deleteHistories() {
+
+    }
+
+    @Override
+    public void doSearch(String keyword) {
+
+    }
+
+    @Override
+    public void research() {
+
+    }
+
+    @Override
+    public void loadMore() {
+
+    }
+
+    @Override
+    public void getRecommendWords() {
+
+    }
+
+    @Override
+    public void registerCallback(ISearchCallback callback) {
+
+    }
+
+    @Override
+    public void unregisterCallback(ISearchCallback callback) {
+
+    }
+}
